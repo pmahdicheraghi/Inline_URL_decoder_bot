2cd017e6d5e8026ac363896ce8c9759754523d7c
pmcheraghi <pmahdicheraghi@gmail.com>
2021-04-06 13:22:24 +0430

    buffer overflow handled

--- index.js
+++ index.js
@@ -4,31 +4,58 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.on('inline_query', async (ctx, next) => {
 
-	let decoded_link = "Please wait ...";
-	console.log(ctx.inlineQuery.query);
-	if (ctx.inlineQuery.query) 
+	let decoded_link = ctx.inlineQuery.query;
+	let err_handler = true
+	if (ctx.inlineQuery.query)
 	{
-		try {
-			decoded_link = decodeURI(ctx.inlineQuery.query);
-			console.log(decoded_link);
+		if (ctx.inlineQuery.query.length == 255) 
+		{
+			while(err_handler)
+			{
+				try {
+					decoded_link = decodeURI(decoded_link);
+					err_handler = false;
+				}
+				catch (e){
+					decoded_link = decoded_link.slice(0, -1);
+				}
+			}
 			ctx.answerInlineQuery([{
 				type: 'article',
 				id: 'someID',
-				title: 'Result Found!',
-				description: decoded_link,
+				title: 'Buffer overflow!',
+				description: "Input link must have upto 254 character\nTry to guess",
 				message_text: decoded_link
 			}]).catch(err => console.log(err.response.description));
-
-		} catch (e) {
-			console.error(e);
-			ctx.answerInlineQuery([{
-				type: 'article',
-				id: 'someID',
-				title: 'Result Not Found!',
-				description: 'URI malformed',
-				message_text: 'URI malformed'
-			}]).catch(err => console.log(err.response.description));
+			console.log("Buffer overflow occurred");
 		}
+		else {
+			try {
+				decoded_link = decodeURI(ctx.inlineQuery.query);
+				ctx.answerInlineQuery([{
+					type: 'article',
+					id: 'someID',
+					title: 'Successfully Decoded!',
+					description: "Result:\n" + decoded_link,
+					message_text: decoded_link
+				}]).catch(err => console.log(err.response.description));
+				console.log("link successfully decoded");
+
+			} catch (e) {
+				console.error(e);
+				ctx.answerInlineQuery([{
+					type: 'article',
+					id: 'someID',
+					title: 'ERROR! Result Not Found!',
+					description: 'Description:\nURI malformed',
+					message_text: 'NO match found!'
+				}]).catch(err => console.log(err.response.description));
+			}
+		}
+	}
+	else
+	{
+		console.log("Empty query entered");
 	}
 });
 
