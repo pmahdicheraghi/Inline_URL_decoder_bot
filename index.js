b81ab8b5fcfdfe8793d8f16b8190849a95cd5194
pmcheraghi <pmahdicheraghi@gmail.com>
2021-04-08 14:32:13 +0430

    cleaning the code

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import { Telegraf, Markup } from 'telegraf';
+import { Telegraf } from 'telegraf';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.on('inline_query', async (ctx, next) => {
-
-	let decoded_link = ctx.inlineQuery.query;
-	let err_handler = true
+bot.on('inline_query', async (ctx) => {
+	let input_link = ctx.inlineQuery.query;
 	if (ctx.inlineQuery.query)
 	{
-		if (ctx.inlineQuery.query.length == 255) 
+		if (ctx.inlineQuery.query.length == 255)
 		{
-			while(err_handler)
+			while (err_handler)
 			{
-				try {
-					decoded_link = decodeURI(decoded_link);
+				try
+				{
+					input_link = decodeURI(input_link);
 					err_handler = false;
 				}
-				catch (e){
-					decoded_link = decoded_link.slice(0, -1);
+				catch (e)
+				{
+					input_link = input_link.slice(0, -1);
 				}
 			}
 			ctx.answerInlineQuery([{
@@ -25,24 +25,27 @@ bot.on('inline_query', async (ctx, next) => {
 				id: 'someID',
 				title: 'Buffer overflow!',
 				description: "Input link must have upto 254 character\nTry to guess",
-				message_text: decoded_link
+				message_text: input_link
 			}]).catch(err => console.log(err.response.description));
 			console.log("Buffer overflow occurred");
 		}
-		else {
-			try {
-				decoded_link = decodeURI(ctx.inlineQuery.query);
+		else
+		{
+			try
+			{
+				input_link = decodeURI(input_link);
 				ctx.answerInlineQuery([{
 					type: 'article',
 					id: 'someID',
 					title: 'Successfully Decoded!',
-					description: "Result:\n" + decoded_link,
-					message_text: decoded_link
+					description: "Result:\n" + input_link,
+					message_text: input_link
 				}]).catch(err => console.log(err.response.description));
 				console.log("link successfully decoded");
-
-			} catch (e) {
-				console.error(e);
+			}
+			catch (e)
+			{
+				console.error(e.message);
 				ctx.answerInlineQuery([{
 					type: 'article',
 					id: 'someID',
@@ -59,9 +62,9 @@ bot.on('inline_query', async (ctx, next) => {
 	}
 });
 
-bot.on("dice", (ctx) => {
-	ctx.reply("Value: " + ctx.message.dice.value, { reply_to_message_id: ctx.message.message_id });
-});
+// bot.on("dice", (ctx) => {
+// 	ctx.reply("Value: " + ctx.message.dice.value, { reply_to_message_id: ctx.message.message_id });
+// });
 
 // bot.on("message", async (ctx) => {
 // 	ctx.replyWithDice({ emoji: "🎯" });
